5ea057cc44496026b2ebeac48c646d9a348bed33
bjroden <bjroden@uark.edu>
2023-01-31 20:45:21 -0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+// TODO: Delete this file. This exists to ensure a working build system before iterative changes start.
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
