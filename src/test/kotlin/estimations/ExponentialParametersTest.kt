bfdf039995e6bc817590d63a403e68d9bfd5dc0c
bjroden <brianjaredroden@gmail.com>
2023-02-03 12:23:40 -0600

    Add exponential parameter test from R groundbeef data

--- src/test/kotlin/estimations/ExponentialParametersTest.kt
+++ src/test/kotlin/estimations/ExponentialParametersTest.kt
@@ -1,12 +1,16 @@
 package estimations
 
+import ksl.utilities.math.KSLMath
 import kotlin.test.Test
-import kotlin.test.fail
 
 class ExponentialParametersTest {
+    // Using digits = 10 in R
+    val groundBeefRate = 0.01357853096
+    val groundBeefMean = 1 / groundBeefRate
 
     @Test
     fun estimate() {
-        fail("Not yet implemented")
+        val params = ExponentialParameters().estimate(Data.groundBeef).getOrThrow()
+        assert(KSLMath.equal(params[0], groundBeefMean)) { "Mean should be $groundBeefMean, was ${params[0]}" }
     }
 }
