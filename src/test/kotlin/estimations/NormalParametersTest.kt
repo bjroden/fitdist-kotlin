b723bfd43fdab51716ec874ef3006f5cce358373
bjroden <brianjaredroden@gmail.com>
2023-02-03 12:20:16 -0600

    Add normal parameter test from R groundbeef data

--- src/test/kotlin/estimations/NormalParametersTest.kt
+++ src/test/kotlin/estimations/NormalParametersTest.kt
@@ -1,12 +1,20 @@
 package estimations
 
+import Data
+import ksl.utilities.math.KSLMath
+import kotlin.math.pow
 import kotlin.test.Test
-import kotlin.test.fail
 
 class NormalParametersTest {
+    // Using digits = 10 in R
+    val groundBeefMean = 73.64566929
+    val groundBeefStdDev = 35.81415889
+    val groundBeefVariance = groundBeefStdDev.pow(2)
 
     @Test
     fun estimate() {
-        fail("Not yet implemented")
+        val params = NormalParameters().estimate(Data.groundBeef).getOrThrow()
+        assert(KSLMath.equal(params[0], groundBeefMean)) { "Mean should be $groundBeefMean, was ${params[0]}" }
+        assert(KSLMath.equal(params[1], groundBeefVariance)) { "Variance should be $groundBeefVariance, was ${params[1]}" }
     }
 }
