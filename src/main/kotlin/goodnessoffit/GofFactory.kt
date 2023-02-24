f3ecb84fccabc8d5bd3846a7f5982de30ec4b6ca
bjroden <bjroden@uark.edu>
2023-02-23 22:06:00 -0600

    Chi square interval creation

--- src/main/kotlin/goodnessoffit/GofFactory.kt
+++ src/main/kotlin/goodnessoffit/GofFactory.kt
@@ -3,6 +3,7 @@ package goodnessoffit
 import ksl.utilities.distributions.CDFIfc
 import ksl.utilities.distributions.ContinuousDistributionIfc
 import ksl.utilities.distributions.DiscreteDistributionIfc
+import ksl.utilities.statistic.Histogram
 
 public class GofFactory {
     public fun <T: AbstractGofTest> continuousTest(
@@ -35,8 +36,30 @@ public class GofFactory {
             dist: CDFIfc,
             request: ChiSquareRequest
         ): ChiSquareGofTest {
-            TODO()
+            val bins = request.breakPoints ?: automaticBins(data)
+            val observedCounts = countObserved(data, bins)
+            val expectedCounts = countExpected(data, dist, bins)
+            return ChiSquareGofTest(expectedCounts, observedCounts)
         }
+
+        private fun countObserved(data: DoubleArray, bins: DoubleArray): DoubleArray {
+            // Use same method of histogram collection as R:
+            // lower < value <= higher, with min value going to first bucket
+            val counts = bins.toList().zipWithNext { lower, upper ->
+                    data.count { lower < it && it <= upper  }.toDouble()
+                }.toMutableList()
+            counts[0] = counts[0] + data.count { it == bins.min() }
+            return counts.toDoubleArray()
+        }
+
+        private fun countExpected(data: DoubleArray, dist: CDFIfc, bins: DoubleArray): DoubleArray {
+            val n = data.size
+            return bins.toList().zipWithNext { lower, upper ->
+                n * ( dist.cdf(upper) - dist.cdf(lower) )
+            }.toDoubleArray()
+        }
+
+        private fun automaticBins(data: DoubleArray) = Histogram.recommendBreakPoints(data)
 
         private fun makeKs(data: DoubleArray, dist: ContinuousDistributionIfc): KolmogorovSmirnovGofTest {
             val cdfs = data.sortedArray().map { dist.cdf(it) }.toDoubleArray()
--- src/main/kotlin/goodnessoffit/GofFactoryOptions.kt
+++ src/main/kotlin/goodnessoffit/GofFactoryOptions.kt
@@ -5,9 +5,8 @@ public sealed interface GofRequest <T: AbstractGofTest>
 public sealed interface ContinuousRequest <T: AbstractGofTest> : GofRequest<T>
 public sealed interface DiscreteRequest <T: AbstractGofTest> : GofRequest<T>
 
-public data class ChiSquareRequest(
-    // TODO: More granular interval definition
-    public val intervals: Int? = null
+public class ChiSquareRequest(
+    public val breakPoints: DoubleArray? = null
 ): ContinuousRequest<ChiSquareGofTest>, DiscreteRequest<ChiSquareGofTest>
 
 public object KSRequest : ContinuousRequest<KolmogorovSmirnovGofTest>
