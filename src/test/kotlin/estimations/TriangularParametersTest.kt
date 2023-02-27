998dd595ed8d497604f1a6a0773b8d556c2a6310
bjroden <brianjaredroden@gmail.com>
2023-02-27 13:39:37 -0600

    Adjust test style

--- src/test/kotlin/estimations/TriangularParametersTest.kt
+++ src/test/kotlin/estimations/TriangularParametersTest.kt
@@ -4,11 +4,9 @@ import ksl.utilities.math.KSLMath
 import ksl.utilities.random.rvariable.TriangularRV
 import org.junit.jupiter.api.Test
 
-//TODO: Fix the tests to align with the new implementation
 class TriangularParametersTest {
-    //The groundBeef and toxocara tests have been (temporarily?) removed due to the fact that input analyzer uses a
-    //Different method of estimation than this. See citation below.
-    //TODO citation
+    // The groundBeef and toxocara tests have been (temporarily?) removed due to the fact that input analyzer uses a
+    // different method of estimation than this.
 
     @Test
     fun estimateRV(){
@@ -27,25 +25,25 @@ class TriangularParametersTest {
         data = TriangularRV(min, mode, max, Data.rvTestStream).sample(sampleSize)
         params = estimator.estimate(data).getOrThrow()
         assert(KSLMath.equal(params[1], mode, precision = Data.defaultRVTestTolerance))
-        { "Mode should be $mode, was ${params[1]}" }
+            { "Mode should be $mode, was ${params[1]}" }
 
-         mode = 50.0
-         data = TriangularRV(min, mode, max, Data.rvTestStream).sample(sampleSize)
-         params = estimator.estimate(data).getOrThrow()
+        mode = 50.0
+        data = TriangularRV(min, mode, max, Data.rvTestStream).sample(sampleSize)
+        params = estimator.estimate(data).getOrThrow()
         assert(KSLMath.equal(params[1], mode, precision = Data.defaultRVTestTolerance))
-        { "Mode should be $mode, was ${params[1]}" }
+            { "Mode should be $mode, was ${params[1]}" }
 
-         mode = 80.0
-         data = TriangularRV(min, mode, max, Data.rvTestStream).sample(sampleSize)
-         params = estimator.estimate(data).getOrThrow()
+        mode = 80.0
+        data = TriangularRV(min, mode, max, Data.rvTestStream).sample(sampleSize)
+        params = estimator.estimate(data).getOrThrow()
         assert(KSLMath.equal(params[1], mode, precision = Data.defaultRVTestTolerance))
-        { "Mode should be $mode, was ${params[1]}" }
+            { "Mode should be $mode, was ${params[1]}" }
 
         mode = 100.0
         data = TriangularRV(min, mode, max, Data.rvTestStream).sample(sampleSize)
         params = estimator.estimate(data).getOrThrow()
         assert(KSLMath.equal(params[1], mode, precision = Data.defaultRVTestTolerance))
-        { "Mode should be $mode, was ${params[1]}" }
+            { "Mode should be $mode, was ${params[1]}" }
     }
 
 }
