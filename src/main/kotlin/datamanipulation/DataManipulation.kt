a8293fd153edafb58588e59c9a93028066139c5a
bjroden <brianjaredroden@gmail.com>
2023-02-28 14:44:58 -0600

    Tweaks for data manipulation Use object instead of class Style / whitespace tweaks

--- src/main/kotlin/datamanipulation/DataManipulation.kt
+++ src/main/kotlin/datamanipulation/DataManipulation.kt
@@ -1,12 +1,12 @@
-package dataManipulation
+package datamanipulation
 
 import ksl.utilities.subtractConstant
 import kotlin.math.floor
 
-public class DataManipulation {
+public object DataManipulation {
     public fun shiftData(data: DoubleArray, zeroPoint: Double = 0.0) : ShiftDataResult {
         val dataCopy = data.copyOf()
-        val offset = (floor(data.min()) - floor(zeroPoint))
+        val offset = floor(data.min()) - floor(zeroPoint)
         val shiftedData = dataCopy.subtractConstant(offset)
         return ShiftDataResult(shiftedData, offset)
     }
